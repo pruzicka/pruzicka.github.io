2b6025ad20edb232cc62dc88184f6ba52b0e3b5b
Petr Ruzicka <pruzicka@gmail.com>
2015-06-17 22:46:32 +0200

    Built blog from commit cf00715 on branch master

--- worker.js
+++ worker.js
@@ -3,7 +3,6 @@ importScripts('/assets/js/serviceworker-cache-polyfill.js');
 var CACHE_NAME = 'pr-cache-v1';
 var url_to_cache = [
 '/',
-'/blog/podcasty-co-vas-zvednou-ze-zidle/index.html',
 '/blog/ido-portal-movement-camp-2015-cast-dve/index.html',
 '/blog/ido-portal-movement-camp-2015-cast-jedna//index.html',
 '/assets/js/search.min.js'
