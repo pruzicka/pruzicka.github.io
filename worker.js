da6592d107c1efafd22a4149c2f679d9e0f760ba
Petr Ruzicka <pruzicka@gmail.com>
2015-06-17 23:06:06 +0200

    Built blog from commit b3efad8 on branch master

--- worker.js
+++ worker.js
@@ -21,7 +21,7 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      return response || return fetch(event.request);
+      return response || fetch(event.request);
     })
     );
 });
