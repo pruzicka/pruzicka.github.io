61f59e56d8c33f309258d15da192d8b6ea3ec84d
Petr Ruzicka <pruzicka@gmail.com>
2015-06-17 22:29:26 +0200

    Built blog from commit dac2d32 on branch master

--- worker.js
+++ worker.js
@@ -1,22 +1,30 @@
 importScripts('/assets/js/serviceworker-cache-polyfill.js');
 
+var CACHE_NAME = 'pr-cache-v1';
+var url_to_cache = [
+'/',
+'/blog/podcasty-co-vas-zvednou-ze-zidle/index.html',
+'/assets/js/search.min.js'
+]
+
 // example usage:
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('pr-cache-v1').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/blog/podcasty-co-vas-zvednou-ze-zidle/',
-        '/assets/js/search.min.js'
-      ]);
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log('Opened cache');
+      return cache.addAll(url_to_cache);
     })
-  );
+    );
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+      if (response)  {
+        console.log('Served from cache');
+        return response;
+      }
+      return fetch(event.request);
     })
-  );
+    );
 });
