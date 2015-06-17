61f2b491760ce0547d9ed0353cacb0200f41d8d6
Petr Ruzicka <pruzicka@gmail.com>
2015-06-17 22:07:24 +0200

    Built blog from commit 4fafd82 on branch master

--- worker.js
+++ worker.js
@@ -3,16 +3,20 @@ importScripts('/assets/js/serviceworker-cache-polyfill.js');
 // example usage:
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('demo-cache').then(function(cache) {
-      return cache.put('/', new Response("From the cache!"));
+    cachesPolyfil.open('pr-cache-v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/blog/podcasty-co-vas-zvednou-ze-zidle/',
+        '/assets/js/search.min.js'
+      ]);
     })
   );
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || new Response("Nothing in the cache for this request");
+    cachesPolyfill.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
